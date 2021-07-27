711fd19a93056cf7b27ff10102ad2f573e65da7b
Chipmunk-g4 <eunchan9029@postech.ac.kr>
2021-07-27 17:30:19 +0900

    727

--- playground.py
+++ playground.py
@@ -1,13 +1,36 @@
-# https://www.acmicpc.net/problem/2293
+# https://www.acmicpc.net/problem/1707
 
-n, k = map(int,input().split())
-nl = [int(input()) for _ in range(n)]
-dp = [0]*(k+1)
-dp[0] = 1
+import sys
+from collections import deque
 
-for i in nl:
-    for j in range(1, k+1):
-        if j-i >= 0:
-            dp[j] += dp[j-i]
+t = int(input())
+for _ in range(t):
+    v, e = map(int,input().split())
+    l = {}
+    for i in range(1,v+1):
+        l[i] = set()
+    for _ in range(e):
+        a, b = map(int,sys.stdin.readline().split())
+        l[a].add(b)
+        l[b].add(a)
+    
+    graph = [-2]*(v+1)
+    result = True
+    for i in range(1,v+1):
+        if graph[i] == -2:
+            q = deque()
+            q.append(i)
+            graph[i] = 1
 
-print(dp[k])
+            while q:
+                now = q.popleft()
+                for c in l[now]:
+                    if graph[c] == graph[now]:
+                        result = False
+                        break
+                    elif graph[c] == -2:
+                        graph[c] = -graph[now]
+                        q.append(c)
+        if result == False:
+            break
+    print('YES' if result else 'NO')
